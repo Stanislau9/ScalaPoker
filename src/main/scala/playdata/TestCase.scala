d796e95f874a21185476304a3ae8ded82bb82e02
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-03-17 14:04:50 +0300

    Fix errors messages in TestCase

--- src/main/scala/playdata/TestCase.scala
+++ src/main/scala/playdata/TestCase.scala
@@ -1,21 +1,26 @@
 package playdata
+
 import card.Card
+import errors.Errors
+import errors.Errors._
 import playdata.Hand._
 
 final case class TestCase(board: Board, hands: List[Hand]) {
-  def create: Either[String, TestCase] = {
+
+  def create(board: Board, hands: List[Hand]): Either[Errors, TestCase] = {
     val cards: List[Card] = {
       (board match {
         case Board(c1, c2, c3, c4, c5) => List(c1, c2, c3, c4, c5)
-        case _                         => Nil
-      }) ++ hands.flatMap {
-        case TexasHand(c1, c2, c3)     => List(c1, c2, c3)
-        case OmahaHand(c1, c2, c3, c4) => List(c1, c2, c3, c4)
-        case _                         => Nil
-      }
+      }) ++
+        hands.flatMap {
+          case TexasHand(c1, c2, c3)     => List(c1, c2, c3)
+          case OmahaHand(c1, c2, c3, c4) => List(c1, c2, c3, c4)
+          case _                         => Nil
+        }
     }
-    if (hands.map(x => x.isInstanceOf[OmahaHand]).distinct.length != 1) Left("Not valid hands")
-    else if (cards != cards.distinct) Left("Not valid cards")
+    if (hands.map(x => x.isInstanceOf[OmahaHand]).distinct.length != 1) Left(NotValidSet)
+    else if (cards != cards.distinct) Left(NotValidSet)
     else Right(TestCase(board, hands))
   }
+
 }
