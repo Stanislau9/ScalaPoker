25fc5df2e366020a7086a57e7be7b5a8c77f4b0a
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-03-17 16:30:47 +0300

    clean

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -1,7 +1,10 @@
+import errors.Errors
+import playdata.TestCase
+
 object Solver extends App {
 
-//  def solve(board: Board, hands: List[Hand]): Result = ???
-//  def solve2(board: Board, hand: Hand): Combination  = ???
-//  def fromStringToCard(string: String): Hand         = ???
+  val a: Either[Errors, TestCase] =
+    TestCase.create("5c6dAcAsQs TsQh9hQc 8d7cTcJd 5s5d7s4d Qd3cKs4c KdJs2hAh Kh4hKc7h 6h7d2cJc")
+  println(a)
 
 }
