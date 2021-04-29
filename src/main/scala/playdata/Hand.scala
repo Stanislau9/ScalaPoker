678012976aaa834c2b883910b46342826e66084e
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-04-29 16:43:13 +0300

    add five card hand

--- src/main/scala/playdata/Hand.scala
+++ src/main/scala/playdata/Hand.scala
@@ -4,5 +4,6 @@ import card.Card
 
 sealed trait Hand
 
-case class TexasHand(c1: Card, c2: Card)                     extends Hand
-case class OmahaHand(c1: Card, c2: Card, c3: Card, c4: Card) extends Hand
+case class TexasHand(c1: Card, c2: Card)                                  extends Hand
+case class OmahaHand(c1: Card, c2: Card, c3: Card, c4: Card)              extends Hand
+case class FiveCardHand(c1: Card, c2: Card, c3: Card, c4: Card, c5: Card) extends Hand
